44d207d493d70ff63835c943b6d0481d4e6084b4
Facu <facundodanielbenitez@gmail.com>
2024-10-14 20:07:02 +0200

    14/10,hasta el ejercicio11

---
+++ Practicas/Tema4/ejArrays+.js
@@ -0,0 +1,28 @@
+function ej8(array) {
+  let array2 = [];
+  let suma = 0;
+  for (let q = 0; q < array.length; q++) {
+    array2.push((suma += array[q]));
+  }
+  console.log(array2);
+  return array2;
+}
+function countBy(x, y) {
+  let array = [];
+  if ((x, (y) => 0)) {
+    for (let a = 1; a <= y; a++) {
+      array.push(x * a);
+    }
+  }
+  console.log(array);
+  return array;
+}
+
+function ej11() {
+  let txt = prompt("Introduce el texto a revertir:");
+  let inicio = prompt("Ahora introduce la palabra que quieras meter:");
+  array = txt.split(" ");
+  array.unshift(array.reverse(), inicio);
+  
+  alert(array);
+}
--- Practicas/Tema4/ejArrays.js
+++ Practicas/Tema4/ejArrays.js
@@ -1,22 +1,133 @@
 function sumaArrays() {
   alert("Introduce 5 numeros:");
-  array = [];
+  let array = [];
   var total = 0;
   for (var i = 0; i < 5; i++) {
     array[i] = parseFloat(prompt("Introduce el numero " + (i + 1)));
-    total += array[i];
+    if (!Number.isInteger(array[i])) {
+      alert("Introduce un numero!");
+      i--;
+    } else {
+      total += array[i];
+    }
   }
   alert("El total es " + total);
   return array;
 }
 
-function maxArrays(){
-    var array2=sumaArrays();
-    let max=0;
-    for(let j=0;j<array2.length;j++){
-        if(max<array2[j]){
-            max=array2[j];
-        }
+function maxArrays() {
+  var array2 = sumaArrays();
+  let max = 0;
+  for (let j = 0; j < array2.length; j++) {
+    if (max < array2[j]) {
+      max = array2[j];
     }
-    alert("Y el maximo es "+max);
+  }
+  alert("Y el maximo es " + max);
+}
+function contarElementos(sumaArrays) {
+  let num;
+  let contador = 0;
+  while (!Number.isInteger(num)) {
+    num = parseFloat(prompt("Ahora escribe el numero que quieres buscar:"));
+    if (!Number.isInteger(num)) {
+      alert("Escriba un numero!");
+    }
+  }
+  for (let k = 0; k < sumaArrays.length; k++) {
+    if (num == sumaArrays[k]) {
+      contador++;
+    }
+  }
+  alert(`Además el número ${num} aparece ${contador} veces`);
+}
+
+function todosPares(array) {
+  let todosPares;
+  for (let l = 0; l < array.length; l++) {
+    if (array[l] % 2 == 0) {
+      todosPares = true;
+    } else {
+      todosPares = false;
+      break;
+    }
+  }
+  if (todosPares) {
+    alert("Y todos los numeros de la array son pares");
+  } else {
+    alert("Y hay al menos un numero impar en la array");
+  }
+}
+function myScript() {
+  let num = 1; //Ponemos uno para que entre la primera vez en el bucle while,a partir de ella el usuario puede modificarlo y terminar
+  let contador = 0;
+  let array = [];
+  let arrayTxt = "";
+  let pares = 0;
+  let impares = 0;
+  while (Number.isInteger(num) && num != 0 && !Number.isNaN(num)) {
+    num = parseFloat(
+      prompt(`Introduce el elemento ${contador + 1} de la array`)
+    );
+    array.push(num);
+    if (num == 0) {
+      array.pop();
+    } else if (num % 2 != 0 && !Number.isNaN(num)) {
+      impares++;
+    } else if (num % 2 == 0) {
+      pares++;
+    } else {
+      //Si entra aqui no es un integer ni un Nan
+    }
+    if (Number.isInteger(array[contador])) {
+      arrayTxt += array[contador] + " ";
+    }
+    contador++;
+  }
+  alert(
+    `Has creado la siguente array ${arrayTxt} y tiene los siguentes numeros pares:${pares} e impares ${impares}`
+  );
+}
+function creaArrays() {
+  let array = [];
+  let elemento;
+  let cont = 1;
+  do {
+    elemento = prompt(
+      "Introduce cualquier cosa como elemento " +
+        cont +
+        " ,si es 0 terminara de pedir*"
+    );
+    array.push(elemento);
+    if (array[cont - 1] == 0) {
+      array.pop();
+    }
+    cont++;
+  } while (elemento != 0);
+
+  return array;
+}
+function buscador() {
+  return prompt("Ahora escribe el texto que quieras buscar");
+}
+function ej6(array, letra) {
+  var cont = false;
+  for (let o = 0; o < array.length; o++) {
+    if (letra == array[o]) {
+      cont = true;
+    }
+  }
+  console.log(cont);
+}
+function listar(array) {
+  let txt;
+  for (let p = 0; p < array.length; p++) {
+    if (p != array.length - 1) {
+      txt += array[p] + "-";
+    } else {
+      txt += array[p];
+    }
+  }
+  alert(txt);
+  return txt;
 }
