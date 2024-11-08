d4259a562ea059a5578e2536457d68ff7189b9fa
Facu <facundodanielbenitez@gmail.com>
2024-11-08 18:38:51 +0100

    clase 8/11

---
+++ Practicas/Tema5/temp.js
@@ -0,0 +1,42 @@
+window.onload=function(){
+    function modificarTabla(){
+
+    }
+    function cambiarTexto(){
+        let parrafo=document.getElementById("parra");
+        console.log(parrafo.innerHTML);
+        parrafo.innerHTML="<em>Este es el nuevo contenido</em>"
+    }
+    function copiarTexto(){
+        let texto=document.getElementById("texto");
+        let destino=document.getElementById("destino");
+        destino.innerHTML=texto.innerHTML;
+    }
+    function pisarTexto(){
+        let parrafo=document.getElementById("padre");
+        parrafo.textContent="Donde caemos gente";
+    }
+    function cambiarEstTexto(){
+        let clase=document.getElementById("estilo");
+        clase.setAttribute("class","nuevoEstilo");
+
+    }
+    function suma(){
+        let caja= document.getElementById("containerNum");
+        let num=parseInt(caja.textContent);
+        num++;
+        caja.textContent=num;
+    }
+    function resta() {
+        let caja= document.getElementById("containerNum");
+        let num=parseInt(caja.textContent);
+        num--;
+        caja.textContent=num;
+    }
+    window.resta=resta;
+    window.suma=suma;
+    window.cambiarTexto=cambiarTexto;
+    window.copiarTexto=copiarTexto;
+    window.pisarTexto=pisarTexto;
+    window.cambiarEstTexto=cambiarEstTexto;
+}
