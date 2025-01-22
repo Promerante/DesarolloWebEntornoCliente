41a2e14e7af761fdbf7427718848b4ca81b8091b
Facu <facundodanielbenitez@gmail.com>
2025-01-22 20:54:13 +0100

    Clase 22/1

--- Practicas/Tema5/EjDOM4/DOM4.js
+++ Practicas/Tema5/EjDOM4/DOM4.js
@@ -1,15 +1,19 @@
 window.onload = function () {
   const ej14 = document.getElementById("ej14");
-  ej14.addEventListener("mouseover", ejercicio14);
-    ej14.addEventListener("mouseout",tutia);
+  var id ;
+  ej14.addEventListener("mouseenter", function(){
+    id= setInterval(ejercicio14, 1);
+  });
+  ej14.addEventListener("mouseout",function(){
+    window.clearInterval(id)
+  });
   function ejercicio14() {
-    let id = setInterval(mover, 1);
-    function mover() {
-       
+  
+    // console.log("iniciado")
       ej14.innerHTML = "jijijiji";
       let ej14Estilo = getComputedStyle(ej14);
       let izquierda = parseInt(ej14Estilo.getPropertyValue("left"));
       ej14.style.setProperty("left", `${izquierda + 1}px`);
-    }
+    
   }
 };
