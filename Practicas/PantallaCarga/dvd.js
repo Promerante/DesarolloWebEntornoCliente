10f6709ce41d3f714e6aabfdffe3b9b522875c3d
Facu <facundodanielbenitez@gmail.com>
2025-02-05 20:48:48 +0100

    DVD falta pulir

--- Practicas/PantallaCarga/dvd.js
+++ Practicas/PantallaCarga/dvd.js
@@ -1,65 +1,74 @@
 window.onload = function () {
+  var screenX = window.innerWidth;
+  var screenY = window.innerHeight;
   var dvd = document.getElementById("dvd");
-  dvd.addEventListener("click", function () {
-    desaparecerTitulo();
-    var id = setInterval(function () {
-      var estado = estado(dvd);
-    }, 1);
-  });
-  function desaparecerTitulo() {
-    var titulo = document.getElementById("titulo");
-    titulo.style.display = "none";
-  }
-  function estado(dvd) {
-    var datos = [];
-    datos[0] = parseFloat(
-      window.getComputedStyle(dvd).getPropertyValue("left")
-    );
-    // datos 0 posicion X
-    datos[1] = parseFloat(window.getComputedStyle(dvd).getPropertyValue("top"));
-    // datos 1 posicion Y
+  var img = ["img/dvd2.png", "img/dvd3.png", "img/dvd.png","img/dvd4.png"];
 
-    datos[2] = Math.floor(Math.random() * 2) + 1;
-    // datos 2 modulo velocidad
-    var borde = false;
-    var caso;
-    if (posX <= 0) {
-      // Caso toca borde izquierdo
-      borde = true;
-      caso = "izquierda";
-    } else if (posX + 151 >= screenX) {
-      // Caso borde derecho, notese que 151 para que concuerde con la parte derecha de la imagen,misma logica para la Y
-      borde = true;
-      caso = "derecha";
+    document.getElementById("titulo").style.display = "none";
+    var modulo = Math.floor(Math.random() * 5) + 1;
+    direccion = Math.PI * (Math.random() * (1 / 2));
+    var posX = 0;
+    var posY = 0;
+    var velocidad = {
+      modulo: modulo,
+      direccion: direccion,
+    };
+    var intervalo = setInterval(function () {
+      var datos = llegaBorde(dvd);
+      if (datos[0]) {
+        velocidad.direccion += Math.PI / 4;
+        switch (datos[1]) {
+          case "izquierda":
+            dvd.style.left = `1px`;
+            break;
+          case "derecha":
+            dvd.style.left = `${screenX-1}px`;
+            break;
+          case "arriba":  
+            dvd.style.top = `0px`;
+            break;
+          case "abajo":  
+            dvd.style.top = `${screenY-500}px`;
+            break;
+      }
+        var color = dvd.getAttribute("src");
+        var index = img.indexOf(color);
+        index = (index + 1) % img.length;
+        dvd.setAttribute("src", img[index]);
+      }
+      posX += velocidad.modulo * Math.cos(velocidad.direccion);
+      posY += velocidad.modulo * Math.sin(velocidad.direccion);
+      dvd.style.left = `${posX}px`;
+      dvd.style.top = `${posY}px`;
+      console.log(datos[0]);
+    }, 1);
+    function llegaBorde(dvd) {
+      var datos = [];
+      var posX = parseFloat(
+        window.getComputedStyle(dvd).getPropertyValue("left")
+      );
+      var posY = parseFloat(
+        window.getComputedStyle(dvd).getPropertyValue("top")
+      );
+      //   Pongo el 5 para que se vea mejor al choca con el borde
+      var tocaBorde = false;
+      if (posX <= 0) {
+        // Caso toca borde izquierdo
+        tocaBorde = true;
+        datos[1] = "izquierda";
+      } else if (posX+151 >= screenX) {
+        // Caso borde derecho, notese que 151 para que concuerde con la parte derecha de la imagen,misma logica para la Y
+        tocaBorde = true;
+        datos[1] = "derecha";
+      }
+      if (posY <= 0) {
+        tocaBorde = true;
+        datos[1] = "arriba";
+      } else if (posY+70 >= screenY) {
+        tocaBorde = true;
+        datos[1] = "abajo";
+      }
+      datos[0] = tocaBorde;
+      return datos;
     }
-    if (posY <= 0) {
-      borde = true;
-      caso = "arriba";
-    } else if (posY + 69 >= screenY) {
-      borde = true;
-      caso = "abajo";
-    }
-    datos[3] = direccion(dvd, borde, caso);
-  }
-  function direccion(dvd, borde, caso) {
-    var direccion;
-    if (typeof direccion == "undefined") {
-      // No existe direccion aun
-      if (!borde) {
-        direccion = Math.PI * (Math.random() * (1 / 2));
-    }else{
-        switch (caso) {
-            case "izquierda":
-                
-                break;
-                case "izquierda":
-                
-                break;
-            default:
-                break;
-        }
-    }
-    }
-    return direccion;
-  }
 };
